9a6173d005b9de4e390243bf238123fc0e46eea3
FelipeSchulzLuz <schulz.neo@gmail.com>
2021-03-02 15:01:50 -0300

    generic routes and validateId on request methods where specify id

--- users/users.router.ts
+++ users/users.router.ts
@@ -1,73 +1,37 @@
-import { Router } from '../common/router'
+import { ModelRouter } from '../common/model-router'
 import * as restify from 'restify'
 import { User } from './users.model'
 import {NotFoundError} from 'restify-errors'
 
 
 
-class UsersRouter extends Router {
+
+class UsersRouter extends ModelRouter<User> {
     constructor(){
-        super()
+        super(User)
         this.on('beforeRender', document=>{
             document.password = undefined
         })
     }
     applyRoutes(application: restify.Server) {
+        
         // Route for get all users on DB
-        application.get('/users', (req, res, next) => {
-            User.find()
-            .then(this.render(res,next))
-            .catch(next)
-        })
+        application.get('/users', this.findAll)
 
         // Route method GET for get an user by ID on DB
-        application.get('/users/:id', (req, res, next) => {
-            User.findById(req.params.id)
-            .then(this.render(res,next))
-            .catch(next)
-        })
+        application.get('/users/:id',[this.validateId, this.findById])
 
         // Route method POST for include a user on DB
-        application.post('/users', (req, res, next) => {
-            let user = new User(req.body)
-            user.save()
-            .then(this.render(res,next))
-            .catch(next)
-        })
+        application.post('/users', this.save)
 
         // Route method PUT for substitute document on DB
-        application.put('/users/:id', (req, res, next) => {
-            const options = {runValidators:true, overwrite: true }
-            User.update({ _id: req.params.id }, req.body, options).exec()
-            .then(result => {
-                if (result.n) {
-                    return User.findById(req.params.id)
-                } else {
-                    throw new NotFoundError('Documento não encontrado')
-                }
-            }).then(this.render(res,next))
-            .catch(next)
-        })
+        application.put('/users/:id', [this.validateId,this.replace])
 
         // Route method PATH, update the document on DB
-        application.patch('/users/:id', (req, res, next) => {
-            const options = { runValidators:true, new: true }
-            User.findByIdAndUpdate(req.params.id, req.body, options)
-            .then(this.render(res,next)).catch(next)
-        });
+        application.patch('/users/:id', [this.validateId,this.update])
 
         // Route method DELETE for delete an document on DB
-        application.del('/users/:id', (req,res,next)=>{
-            User.remove({_id:req.params.id}).exec()
-            .then((cmdResult: any)=>{
-                if(cmdResult.result.n){
-                    res.send(204)
-                }else{
-                    throw new NotFoundError('Documento não encontrado')
-                }
-                return next()
-            }).catch(next)
-        })
+        application.del('/users/:id', [this.validateId,this.delete])
     }
 }
 
